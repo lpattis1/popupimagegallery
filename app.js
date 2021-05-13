b2da5406b692a090e8b9e612ebff7ff00b3fb680
Lauren <lpattis1@asu.edu>
2021-05-13 15:09:54 -0500

    Cleaned up code.

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 const posters = document.querySelectorAll(".img-container > img");
 const modal = document.querySelector(".modal");
 const fullImg = document.querySelector(".full-img");
-const gallery = document.querySelector(".gallery");
-console.log(posters);
+const smallImg = document.querySelector(".img-container > img");
 
 function openImage() {
-  posters.forEach((poster, i) => {
+  posters.forEach((poster) => {
     poster.addEventListener("click", function (e) {
-      let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
       let posterAlt = poster.getAttribute("alt");
-      console.log(posterAlt);
-      if (e.target) {
-        modal.classList.add("open");
-        fullImg.classList.add("enlarge");
-        fullImg.src = `/img/${posterAlt}.jpg`;
-      }
+      modal.classList.add("open");
+      fullImg.classList.add("enlarge");
+      fullImg.src = `/img/${posterAlt}.jpg`;
+    });
 
-      modal.addEventListener("click", function (e) {
-        if (e.target === modal) {
-          modal.classList.remove("open");
-          fullImg.classList.remove("enlarge");
-        }
-      });
+    modal.addEventListener("click", function (e) {
+      if (e.target === modal) {
+        modal.classList.remove("open");
+        fullImg.classList.remove("enlarge");
+      }
     });
   });
 }
